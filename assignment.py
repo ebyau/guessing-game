ddda976c5ef23751ce302956107777b9fc29ad0b
ebyau <38182764+ebyau@users.noreply.github.com>
2018-07-03 07:40:15 -0700

    Add files via upload

--- assignment.py
+++ assignment.py
@@ -1,61 +1,66 @@
 import random
 
 
-def correct_number_wrong_position():
+def play_mind_game():
     first_list = list(range(10))
     random.shuffle(first_list)
     digits = (first_list[:3])
     print(digits)
+    lives = 5
 
     while True:
-        entry = input("Please enter a 3 digit number")
+        entry = input("hello there! please any 3 digit number")
         present = []
         c = 0
+        lives -= 1
+        print(f"{lives} chances left")
+        if lives == 0:
+            print(f"thanks for trying")
+            break
+        else:
+            continue
 
         if len(entry) != 3:
-            print("Please enter a 3 digit number")
+            print("Only 3 digit numbers allowed")
             continue
 
         try:
-            int(entry)/2
+            int(entry) / 3
             entries = list(entry)
             for x in entries:
                 if int(x) in digits:
-                    print(f"{x} is present")
+                    print(f"{x} is a close")
                     present.append(int(x))
                 else:
                     pass
-
 
             if len(present) == 0:
                 print("Nope")
                 continue
             else:
                 for i in present:
-                    index_entries = entries.index(i)
+                    index_entries = entries.index(str(i))
                     index_digits = digits.index(i)
                     if index_digits == index_entries:
-                        print(f"{i} is in the right position")
+                        print(f"{i} is a match ")
                         c += 1
                     else:
-                        print(f"{i} is in the wrong position")
+                        print(f"but {i} is in the wrong position")
+                        continue
 
             if c == 3:
                 print(f"{entry} is the correct number")
                 again = input("Do you want to play again")
                 if again == "yes":
-                    correct_number_wrong_position()
+                    play_mind_game()
                 else:
-                    print("Thanks bye")
+                    print("Thanks bye see ya!")
                     break
             else:
                 continue
         except ValueError:
-            print("Please enter a 3 digit number")
+            print("Only 3 digit numbers are allowed")
             continue
 
 
-
-
-
-correct_number_wrong_position()
+play_mind_game()
